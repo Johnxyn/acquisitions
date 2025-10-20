223b083a482a3f7da1f544d4197b97d4df9945aa
John Ohaeri <Johnxyn@yahoo.com>
2025-10-20 23:27:01 +0200

    Merge remote history with local

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 
 const app = express();
@@ -7,13 +6,5 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello from Acquisitions!');
 });
 export default app;
-=======
-import express from 'express';
 
-const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello from Acquisitions!@@!');
-});
-export default app;
->>>>>>> 2fc66ed142d763047a86f021708c7b980013b47f
--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 import 'dotenv/config';
 import './server.js';
-=======
-import 'dotenv/config'
-import './server.js'
->>>>>>> 2fc66ed142d763047a86f021708c7b980013b47f
